888e75fa0973ba9fa9dfe24156342e1dce0c5b97
yeged <yigityceylan@gmail.com>
2021-11-16 17:59:16 +0300

    Selector Improvement

--- src/components/SelectorsBasic/SelectorsBasic.tsx
+++ src/components/SelectorsBasic/SelectorsBasic.tsx
@@ -1,26 +1,49 @@
 import React from 'react';
-import {
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from 'recoil';
+import { atom, selector, useRecoilState } from 'recoil';
 
 const exchangeRate = 0.097397644;
+
+const addCommission = (amount: number, commission: number) => {
+  return amount / (1 - commission / 100);
+};
+
+const removeCommission = (amount: number, commission: number) => {
+  return amount * (1 - commission / 100);
+};
 
 const tryState = atom<number>({
   key: 'tryState',
   default: 1,
 });
 
+const comissionEnabledState = atom<boolean>({
+  key: 'comissionEnabledState',
+  default: false,
+});
+
+const comissionState = atom<number>({
+  key: 'comissionState',
+  default: 5,
+});
+
 const usdSelector = selector<number>({
   key: 'usdSelector',
   get: ({ get }) => {
-    const lira = get(tryState);
+    let lira = get(tryState);
+    const comissionEnable = get(comissionEnabledState);
+    if (comissionEnable) {
+      const comission = get(comissionState);
+      return removeCommission(lira, comission) * exchangeRate;
+    }
     return lira * exchangeRate;
   },
-  set: ({ set }, newUsdValue) => {
+  set: ({ set, get }, newUsdValue) => {
     let newTrValue = +newUsdValue / exchangeRate;
+    const comissionEnable = get(comissionEnabledState);
+    if (comissionEnable) {
+      const comission = get(comissionState);
+      newTrValue = addCommission(newTrValue, comission);
+    }
     set(tryState, newTrValue);
   },
 });
@@ -28,6 +51,10 @@ const usdSelector = selector<number>({
 const SelectorsBasic = () => {
   const [lira, setLira] = useRecoilState(tryState);
   const [usd, setUSD] = useRecoilState(usdSelector);
+  const [isComissionEnable, setIsComissionEnable] = useRecoilState(
+    comissionEnabledState,
+  );
+  const [comission, setComission] = useRecoilState(comissionState);
 
   return (
     <div>
@@ -48,6 +75,19 @@ const SelectorsBasic = () => {
           onChange={(e) => setUSD(+e.target.value)}
         />
         <label htmlFor="usd">$</label>
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          checked={isComissionEnable}
+          onChange={() => setIsComissionEnable(!isComissionEnable)}
+        />
+        <input
+          placeholder="comission"
+          disabled={!isComissionEnable}
+          value={comission}
+          onChange={(e) => setComission(+e.target.value)}
+        />
       </div>
     </div>
   );
